3530f93807f4faf2631ce25b7e283bae5e09a936
filatkinen <filatkinen@gmail.com>
2023-06-19 20:12:16 +0300

    add test.sh

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
+NC_PID=$!
+
+sleep 1
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | nc  localhost 4242 >/tmp/telnet.out &
+TL_PID=$!
+
+sleep 2
+kill ${TL_PID} 2>/dev/null || true
+kill ${NC_PID} 2>/dev/null || true
+
+function fileEquals() {
+  local fileData
+  fileData=$(cat "$1")
+  [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}" && exit 1)
+}
+
+expected_nc_out='I
+am
+TELNET client'
+fileEquals /tmp/nc.out "${expected_nc_out}"
+
+expected_telnet_out='Hello
+From
+NC'
+fileEquals /tmp/telnet.out "${expected_telnet_out}"
+
+echo "PASS"
